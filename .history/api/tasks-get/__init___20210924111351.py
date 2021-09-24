45392e341d4db7016a37e2673f8288ba35efb424
IssamBouzidi <issam.bouzidi.01@gmail.com>
2021-09-24 11:16:52 +0200

    ci:add get http request

--- .history/api/tasks-get/__init___20210924111351.py
+++ .history/api/tasks-get/__init___20210924111351.py
@@ -40,6 +40,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             req_body,
              status_code=200
         )
