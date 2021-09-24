2eef67f5fbb7237e9a75be205d1ae65df57d7d94
IssamBouzidi <issam.bouzidi.01@gmail.com>
2021-09-24 11:34:17 +0200

    add tasks list

---
+++ .history/api/tasks-get/__init___20210924113138.py
@@ -0,0 +1,46 @@
+import logging
+import json
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            tasks = {
+                [
+                    {
+                        "id": 1,
+                        "label": "🍔 Eat",
+                        "status": ""
+                    },
+                    {
+                        "id": 2,
+                        "label": "🛏 Sleep",
+                        "status": ""
+                    },
+                    {
+                        "id": 3,
+                        "label": "</> Code",
+                        "status": ""
+                    }
+                ]
+            }
+
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+            json.dumps(tasks),
+            mimetype="application/json",
+            status_code=200
+        )
